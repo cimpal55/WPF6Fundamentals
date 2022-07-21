7ad47c6cec263b478a29843c4e047db5f36ca560
cimpal55 <cimpals71@gmail.com>
2022-07-21 15:22:48 +0300

    4 Part finished

--- WPF6Fundamentals/MainWindow.xaml.cs
+++ WPF6Fundamentals/MainWindow.xaml.cs
@@ -29,5 +29,17 @@ namespace WPF6Fundamentals
         {
 
         }
+
+        private void btnMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(customerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(customerListGrid, newColumn);
+        }
     }
 }
